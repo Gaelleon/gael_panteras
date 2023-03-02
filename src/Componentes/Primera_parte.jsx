9f5871a6473180e779485ecc69f20927c1540e20
Gaelleon <gaelleon677@gmail.com>
2023-03-02 00:28:42 -0600

    c

---
+++ src/Componentes/Primera_parte.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import '../index.css'
+function Primera_parte(){
+    return(<div className="Fondo">
+            <h1>panteras</h1>
+            </div>);
+}
+export default Primera_parte;
