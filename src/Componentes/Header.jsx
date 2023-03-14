7e6cc93b492c37173a27f7e6953a79356ad9119a
Gaelleon <gaelleon677@gmail.com>
2023-03-13 21:04:59 -0600

    Cambios

--- src/Componentes/Header.jsx
+++ src/Componentes/Header.jsx
@@ -1,11 +1,26 @@
 import React from "react";
 import '../index.css'
+import Prueba from './Prueba'
+import recuadro from "../recuadros";
 
 function Header() {
     return (
-        <div className="Fondo">
+        <div className="Titulo">
             <h1>Mi Pagina Personal</h1>
-                <body></body>
-        </div>);
+            <body className="Perfil">
+                Gran habilidad y responsabilidad para proyectos, siempre listo para aplicar los conocimientos adquiridos durante la licenciatura con fin de ayudarme a crecer como persona y profesionalmente.
+                {recuadro.map(content => (
+                    <Prueba 
+                    key = {content.key}
+                    titulo = {content.titulo_recuadro}
+                    info1 = {content.informacion1}
+                    info2 = {content.informacion2}
+                    info3 = {content.informacion3}
+                    
+                    />
+                ))}
+            </body>
+        </div>
+    );
 }
 export default Header;
---
+++ src/recuadros.js
@@ -0,0 +1,17 @@
+const recuadro = [
+    {
+    key: 1,
+    titulo_recuadro:"CONTACTO",
+    informacion1:"Telefono: 5525194728",
+    informacion2:"DIRECCIÓN: Calle 4 NO. 320 Col. Del gas Azcapotzalco, 02950, CDMX",
+    informacion3:"CORREO ELECTRÓNICO: gaelleon677@gmail.com"
+    },
+    {
+    key: 2,
+    titulo_recuadro:"APTITUDES",
+    informacion1: "Honesto",
+    informacion2: "Creatividad",
+    informacion3: "Flexibilidad"
+    }
+];
+export default recuadro;
